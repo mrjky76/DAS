fa4f71a598930319966ce40ccfd3d2e80e8a7515
mrjky76 <73655776+mrjky76@users.noreply.github.com>
2020-11-09 02:39:53 +0800

    Update multisender.sol

--- multisender.sol
+++ multisender.sol
@@ -1,48 +1,4 @@
-pragma solidity ^0.4.20;
-
-
-contract TRC20 {
-    // Public variables of the token
-    string public name;
-    string public symbol;
-    uint8 public decimals = 10;
-    // 18 decimals is the strongly suggested default, avoid changing it
-    uint256 public totalSupply;
-
-    // This creates an array with all balances
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
-
-    // This generates a public event on the blockchain that will notify clients
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    // This generates a public event on the blockchain that will notify clients
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-
-    // This notifies clients about the amount burnt
-    event Burn(address indexed from, uint256 value);
-
-    /**
-     * Constructor function
-     *
-     * Initializes contract with initial supply tokens to the creator of the contract
-     */
-    uint256 initialSupply = 1000000000;
-    string tokenName = 'TUMStableCoin';
-    string tokenSymbol = 'TUMS';
-    constructor() public {
-
-        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
-        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
-        name = tokenName;                                   // Set the name for display purposes
-        symbol = tokenSymbol;                               // Set the symbol for display purposes
-    }
-
-    /**
-     * Internal transfer, only can be called by this contract
-     */
-    function _transfer(address _from, address _to, uint _value) internal {
-        // Prevent transfer to 0x0 address. Use burn() insteadpragma solidity ^0.4.25;
+pragma solidity ^0.4.25;
 
 
 /**
